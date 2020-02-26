ab91a59a4d35b93068ed86b65f3e9a4874c710f9
mbovan <mbovan@3149657.no-reply.drupal.org>
2020-02-26 11:14:08 +0100

    Issue #3115613 by mbovan, Berdir: Shipping method formatter does not respect the given langcode parameter

--- tests/src/Kernel/Formatter/ShippingMethodFormatterTest.php
+++ tests/src/Kernel/Formatter/ShippingMethodFormatterTest.php
@@ -5,6 +5,7 @@ namespace Drupal\Tests\commerce_shipping\Kernel\Formatter;
 use Drupal\commerce_shipping\Entity\Shipment;
 use Drupal\commerce_shipping\Entity\ShippingMethod;
 use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\language\Entity\ConfigurableLanguage;
 use Drupal\Tests\commerce_shipping\Kernel\ShippingKernelTestBase;
 
 /**
@@ -15,6 +16,32 @@ use Drupal\Tests\commerce_shipping\Kernel\ShippingKernelTestBase;
  * @group commerce_shipping
  */
 class ShippingMethodFormatterTest extends ShippingKernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'language',
+    'content_translation',
+  ];
+
+  /**
+   * The language used for testing.
+   *
+   * @var \Drupal\Core\Language\LanguageInterface
+   */
+  protected $translationLanguage;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->translationLanguage = ConfigurableLanguage::createFromLangcode('fr');
+    $this->translationLanguage->save();
+    $this->container->get('content_translation.manager')->setEnabled('commerce_shipping_method', 'commerce_shipping_method', TRUE);
+  }
 
   /**
    * Tests the rendered output.
@@ -40,6 +67,19 @@ class ShippingMethodFormatterTest extends ShippingKernelTestBase {
         ],
       ],
     ]);
+    $shipping_method->addTranslation($this->translationLanguage->id(), [
+      'name' => 'Shipping method',
+      'plugin' => [
+        'target_plugin_id' => 'flat_rate',
+        'target_plugin_configuration' => [
+          'rate_label' => 'Test translation shipping',
+          'rate_amount' => [
+            'number' => '10',
+            'currency_code' => 'USD',
+          ],
+        ],
+      ],
+    ]);
     $shipping_method->save();
 
     $shipment = Shipment::create([
@@ -52,6 +92,20 @@ class ShippingMethodFormatterTest extends ShippingKernelTestBase {
       'state' => 'ready',
     ]);
     $shipment->save();
+
+    $default_language = $this->container->get('language_manager')->getDefaultLanguage();
+    $this->container->get('language.default')->set($this->translationLanguage);
+
+    // Confirm that the translated rate_label is used.
+    $view_display = EntityViewDisplay::collectRenderDisplay($shipment, 'default');
+    $build = $view_display->build($shipment);
+    $this->render($build);
+    $this->assertText('Shipping method');
+    $this->assertText('Test translation shipping');
+
+    // Revert the site default language to the original language.
+    $this->container->get('language.default')->set($default_language);
+    $this->container->get('language_manager')->reset();
 
     $view_display = EntityViewDisplay::collectRenderDisplay($shipment, 'default');
     $build = $view_display->build($shipment);
@@ -66,6 +120,7 @@ class ShippingMethodFormatterTest extends ShippingKernelTestBase {
     $this->render($build);
     $this->assertNoText('Shipping method', $this->content);
     $this->assertNoText('Test shipping');
+    $this->assertNoText('Test translation shipping');
   }
 
 }
